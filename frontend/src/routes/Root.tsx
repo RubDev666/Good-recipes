7dd1b88ed6589f7c863b32743fd291f7e18b25da
Ulises-Saucedo <ulisesaucedo.se04@gmail.com>
2025-01-30 16:10:40 -0300

    footer added to main layout

--- frontend/src/routes/Root.tsx
+++ frontend/src/routes/Root.tsx
@@ -1,4 +1,5 @@
 import { Outlet } from "react-router";
+import { Footer } from "../components/Footer";
 
 export default function Root() {
     return(
@@ -7,7 +8,7 @@ export default function Root() {
 
             <Outlet />
 
-            <p>Footer</p>
+            <Footer />
         </>
     )
 }
