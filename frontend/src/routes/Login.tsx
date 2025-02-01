210b1e2459624b4897f1917404237ee556654d51
darkhope22 <mjacoboarias1106@gmail.com>
2025-02-01 17:20:06 -0600

    add login form

--- frontend/src/routes/Login.tsx
+++ frontend/src/routes/Login.tsx
@@ -1,7 +1,41 @@
+import { Form } from "../components/ui/Form/Form"
+import { InputField } from "../components/ui/InputField/InputField"
+
 export default function Login() {
-    return(
+    return (
         <>
-            <p>Login</p>
+            <section className="formContainer">
+                <div className="formImage">
+                    {/* svg background */}
+                </div>
+
+                <Form className="form">
+                    <h3 className="formTitle">Ingresa con tu correo electrónico</h3>
+
+                    <InputField
+                        labelName="Correo electrónico *"
+                        fieldId="email"
+                        fieldName="emailField"
+                        fieldPlaceholder="Correo electrónico*"
+                        fieldType="email"
+                        autoComplete="off"
+                    />
+                    <InputField
+                        labelName="Contraseña *"
+                        fieldId="password"
+                        fieldName="passwordField"
+                        fieldPlaceholder="Contraseña *"
+                        fieldType="password"
+                        autoComplete="off"
+                    />
+
+                    <button type="submit"
+                        className="submitButton"
+                    >
+                        Registrarse
+                    </button>
+                </Form>
+            </section>
         </>
     )
 }
