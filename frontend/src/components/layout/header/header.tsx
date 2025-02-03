ae35d9f386de7798c03a231c82725afe48025435
AndresTapia18 <111100886+AndresTapia18@users.noreply.github.com>
2025-02-03 14:29:49 -0500

    Header terminado

---
+++ frontend/src/components/layout/header/header.tsx
@@ -0,0 +1,81 @@
+import { useState } from "react";
+import { Link } from "react-router";
+import "./Header.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faBars,
+  faTimes,
+  faBowlFood,
+  faMartiniGlassCitrus,
+} from "@fortawesome/free-solid-svg-icons";
+
+const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <header className="header">
+      <div className="container">
+        {/* Logo */}
+        <Link to="/" className="logo">
+          <img
+            src="/icons/apple-touch-icon.png"
+            alt="Logo"
+            className="logo-img"
+          />
+        </Link>
+
+        {/* Menú de navegación */}
+        <nav className="nav">
+          <Link to="/meals" className="nav-link">
+            <FontAwesomeIcon icon={faBowlFood} />
+            Meals
+          </Link>
+          <Link to="/cocktails" className="nav-link">
+            <FontAwesomeIcon icon={faMartiniGlassCitrus} />
+            Cocktails
+          </Link>
+        </nav>
+
+        {/* Módulo Sign In / Sign Up */}
+        {!isOpen ? (
+          <div className="auth">
+            <Link to="/sign-in" className="auth-link">
+              Sign In
+            </Link>
+            <Link to="/sign-up" className="auth-button">
+              Sign Up
+            </Link>
+          </div>
+        ) : (
+          <div className="auth">
+            <Link to="/sign-up" className="auth-button">
+              Sign Up
+            </Link>
+          </div>
+        )}
+
+        {/* Menú móvil */}
+        <button onClick={() => setIsOpen(!isOpen)} className="menu-button">
+          <FontAwesomeIcon icon={isOpen ? faTimes : faBars} size="lg" />
+        </button>
+      </div>
+
+      {/* Menú desplegable en móvil */}
+      <div className={`mobile-menu ${isOpen ? "open" : ""}`}>
+        <Link to="/meals" className="mobile-link">
+          <FontAwesomeIcon icon={faBowlFood} />
+          Meals
+        </Link>
+        <Link to="/cocktails" className="mobile-link">
+          <FontAwesomeIcon icon={faMartiniGlassCitrus} />
+          Cocktails
+        </Link>
+        <Link to="/sign-in" className="sigin">
+          Sign In
+        </Link>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
--- frontend/src/routes/Root.tsx
+++ frontend/src/routes/Root.tsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router";
 import { Footer } from "../components/Footer";
-import Header from "../components/layout/header";
+import Header from "../components/layout/header/header";
 
 export default function Root() {
   return (
