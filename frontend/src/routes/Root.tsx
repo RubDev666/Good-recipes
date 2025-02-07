34c5e20dbd374aede90c15969314b02fc05badb5
Ulises-Saucedo <ulisesaucedo.se04@gmail.com>
2025-02-07 18:11:39 -0300

    fix: header mobile menu open does not exceed viewport

--- frontend/src/routes/Root.tsx
+++ frontend/src/routes/Root.tsx
@@ -1,11 +1,11 @@
 import { Outlet } from "react-router";
-import Header from "../components/layout/header/header";
+import { Header } from "../components/layout/header/header";
 import { Footer } from "../components/layout/footer/Footer";
 
 export default function Root() {
   return (
     <>
-      <h1>Header</h1>
+      <Header />
 
       <Outlet />
 
