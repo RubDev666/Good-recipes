ad2254b5b15a23afedb2ef8d3bbb0520a1cc7503
RubDev666 <125171500+RubDev666@users.noreply.github.com>
2025-02-08 17:25:01 -0600

    old header removed

---
+++ frontend/src/components/layout/header/Header.tsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+    faBars,
+    faTimes,
+    faBowlFood,
+    faMartiniGlassCitrus,
+    faCutlery
+} from "@fortawesome/free-solid-svg-icons";
+
+import Logo from "../../ui/Logo.tsx";
+
+export const Header = () => {
+    return (
+        <p>Header</p>
+    );
+};
--- frontend/src/routes/Root.tsx
+++ frontend/src/routes/Root.tsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router";
-import { Header } from "../components/layout/header/header";
+import { Header } from "../components/layout/header/Header.tsx";
 import { Footer } from "../components/layout/footer/Footer";
 
 export default function Root() {
